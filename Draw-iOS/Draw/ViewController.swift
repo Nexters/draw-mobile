92ab5902d6a20e5bf0615eed2480c4379746ec52
송영모 <mooyoung2309@gmail.com>
2023-07-20 00:33:55 +0900

    feat: 프로젝트 세팅

---
+++ Draw-iOS/Draw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Draw
+//
+//  Created by 송영모 on 2023/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
