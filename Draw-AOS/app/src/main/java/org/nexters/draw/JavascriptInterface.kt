bee6528d881047274e99219a533a0f1152e0240c
안나영 <dpswpfskdud@naver.com>
2023-07-31 18:09:39 +0900

    [feat] shareSheet 구현

--- Draw-AOS/app/src/main/java/org/nexters/draw/JavascriptInterface.kt
+++ Draw-AOS/app/src/main/java/org/nexters/draw/JavascriptInterface.kt
@@ -1,8 +1,11 @@
 package org.nexters.draw
 
+import android.content.Context
+import android.content.Intent
 import android.webkit.JavascriptInterface
+import androidx.core.content.ContextCompat.startActivity
 
-class JavascriptInterface(private val viewModel: MainViewModel) {
+class JavascriptInterface(private val viewModel: MainViewModel, private val context: Context) {
 
     @JavascriptInterface
     fun showBottomBar(showBottom: String) {
@@ -11,5 +14,13 @@ class JavascriptInterface(private val viewModel: MainViewModel) {
         } else {
             viewModel.setBottomBarVisible(false)
         }
+    }
+
+    @JavascriptInterface
+    fun showShareSheet(shareUrl: String) {
+        val intentShare = Intent(Intent.ACTION_SEND)
+        intentShare.putExtra(Intent.EXTRA_TEXT, shareUrl)
+        intentShare.type = "text/plain"
+        startActivity(context, Intent.createChooser(intentShare, "앱을 선택해 주세요."), null)
     }
 }
