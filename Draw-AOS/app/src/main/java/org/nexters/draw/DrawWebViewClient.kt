686497bd41ffa9b8ecd84db12e4c1424178a9ab9
안나영 <dpswpfskdud@naver.com>
2023-08-01 15:06:34 +0900

    [feat] 질문하기 화면 연결

--- Draw-AOS/app/src/main/java/org/nexters/draw/DrawWebViewClient.kt
+++ Draw-AOS/app/src/main/java/org/nexters/draw/DrawWebViewClient.kt
@@ -1,54 +1,27 @@
 package org.nexters.draw
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.graphics.Bitmap
-import android.util.Log
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.core.content.ContextCompat.startActivity
 
 
-open class DrawWebViewClient:WebViewClient() {
-    // 웹 페이지 로딩 시작 시 호출됩니다.
-    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-        super.onPageStarted(view, url, favicon)
-        // 웹 페이지 로딩 시작 시 원하는 동작을 수행합니다.
-    }
+object DrawWebViewClient {
 
-    // 웹 페이지 로딩 완료 시 호출됩니다.
-    override fun onPageFinished(view: WebView?, url: String?) {
-        super.onPageFinished(view, url)
-        // 웹 페이지 로딩 완료 시 원하는 동작을 수행합니다.
-    }
-
-    @Deprecated("Deprecated in Java", ReplaceWith("false"))
-    override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-
-        Log.d("TAG", "shouldOverrideUrlLoading: ${url}")
-        return if (url == BuildConfig.WEB_URL_FEED) {
-            val intent = Intent(view?.context , MainActivity::class.java)
-            startActivity(view?.context!!,intent,null)
-            true
-        }else {
-            false
+    @SuppressLint("SetJavaScriptEnabled")
+    fun initWebView(webView: WebView) {
+        webView.apply {
+            webChromeClient = WebChromeClient()
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.setSupportMultipleWindows(true)
         }
     }
 
-
-
-    // 웹 페이지 로딩 중 에러가 발생할 때 호출됩니다.
-    @Deprecated("Deprecated in Java")
-    override fun onReceivedError(view: WebView?, errorCode: Int, description: String?, failingUrl: String?) {
-        // 에러 핸들링을 수행합니다.
-    }
-
     @SuppressLint("SetJavaScriptEnabled")
-    fun loadUrl(view: WebView, url:String) {
-        view.settings.javaScriptEnabled = true
-        view.settings.domStorageEnabled = true
-        view.settings.setSupportMultipleWindows(true)
-        view.loadUrl(url)
+    fun loadWebUrl(webView: WebView, url: String) {
+        webView.loadUrl(url)
     }
 
 }
--- Draw-AOS/app/src/main/java/org/nexters/draw/FeedFragment.kt
+++ Draw-AOS/app/src/main/java/org/nexters/draw/FeedFragment.kt
@@ -1,11 +1,9 @@
 package org.nexters.draw
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebChromeClient
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import org.nexters.draw.databinding.FragmentFeedBinding
@@ -23,24 +21,23 @@ class FeedFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         _binding = FragmentFeedBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.wbDraw.apply {
-            webChromeClient = WebChromeClient()
-            webViewClient = DrawWebViewClient()
-            settings.javaScriptEnabled = true
-            settings.domStorageEnabled = true
-            settings.setSupportMultipleWindows(true)
-            loadUrl(BuildConfig.WEB_URL_FEED)
+            DrawWebViewClient.run {
+                initWebView(this@apply)
+                loadWebUrl(this@apply, BuildConfig.WEB_URL_FEED)
+            }
+            addJavascriptInterface(
+                JavascriptInterface(mainViewModel, requireContext()),
+                "draw"
+            )
         }
-        binding.wbDraw.addJavascriptInterface(JavascriptInterface(mainViewModel,requireContext()), "draw")
     }
 
     override fun onDestroyView() {
